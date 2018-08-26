4002595379a8f66f52c73af38636d89dc4ea1fd5
dikshant07 <42529140+dikshant07@users.noreply.github.com>
2018-08-26 20:43:15 +0530

    Assignment5

---
+++ Assignment5/Main.java
@@ -0,0 +1,19 @@
+class Test {
+    static int i;
+    int j;
+    static {
+        i = 10;
+        System.out.println("Static block called ");
+    }
+    Test(){
+        System.out.println("Constructor called");
+    }
+}
+ 
+class Main {
+    public static void main(String[] x) {
+ 
+      
+       Test t1 = new Test();
+    }
+}
